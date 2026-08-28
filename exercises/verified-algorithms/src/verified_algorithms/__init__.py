572b02e9b38bb663a1e4ce2594aa24209782abe6
seungwoo7050 <seungwoo7050@naver.com>
2026-08-28 15:48:16 +0900

    feat: verified-algorithms 공개 API 구성

--- exercises/verified-algorithms/src/verified_algorithms/__init__.py
+++ exercises/verified-algorithms/src/verified_algorithms/__init__.py
@@ -1,1 +1,26 @@
-"""Algorithm implementation package."""
+"""Stable public API for the verified-algorithms package."""
+
+# [Implementation 13]
+# Public API composition
+from .graphs import bellman_ford, bfs_distances, dijkstra, kruskal_mst, max_flow
+from .optimization import knapsack_01, lcs_length, select_intervals
+from .ranges import lower_bound, prefix_sums, range_sum
+from .strings import kmp_find
+from .trees import RedBlackNode, red_black_height
+
+__all__ = [
+    "RedBlackNode",
+    "bellman_ford",
+    "bfs_distances",
+    "dijkstra",
+    "kmp_find",
+    "knapsack_01",
+    "kruskal_mst",
+    "lcs_length",
+    "lower_bound",
+    "max_flow",
+    "prefix_sums",
+    "range_sum",
+    "red_black_height",
+    "select_intervals",
+]
